13f7714665095e1d28893340207f3394c6352982
Andy Lee <alee@Andys-MacBook-Pro.local>
2015-04-06 16:38:15 -0700

    fix invitations controller

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,8 +1,7 @@
-class Users::InvitationsController < Devise::InvitationsController
+class User::InvitationsController < Devise::InvitationsController
   def update
     @user = User.find params[:id]
     @user.update_attributes params[:user]
-    binding.pry
     redirect_to root_path
 
   end
