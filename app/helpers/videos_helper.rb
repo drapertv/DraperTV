0c01727a2d44d3e472a76523506a529d8007132b
Hubert <hubert.pt.ka@gmail.com>
2015-04-27 18:02:38 -0700

    update livestream font sizes, spacing, and banner

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -11,7 +11,7 @@ module VideosHelper
 
 	  def expandable_text text
 	  	if text.length > 750
-	  		"#{text[0..749]}<span class='show-more'>&nbsp;Show More</span><span class='hidden'> #{text[750..-1]}&nbsp;<span class='show-less'>Show Less</span></span>".html_safe
+	  		"#{text[0..349]}&nbsp;&nbsp;&nbsp;<span class='show-more'>Show more...</span><span class='hidden'> #{text[350..-1]}&nbsp;&nbsp;&nbsp;<span class='show-less'>Show less</span></span>".html_safe
 	  	else
 	  		text
 	  	end
