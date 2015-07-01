8fb8cde23debab3363bf9d6d8f10d3a97c5d6e85
Andy Lee <alee@andy.local>
2015-07-01 11:57:19 -0700

    remove pagination, make categories always visible

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,36 +4,18 @@ class SearchesController < ApplicationController
     @terms = params[:search][:terms]
     Search.create terms: @terms
 
-    @og_title = "Search for \"#{@terms}\" - DraperTV"
+    @og_title = "DraperTV - Search for \"#{@terms}\""
     
-    @page = params[:page] || 1
-    @page = @page.to_i
-  	@page_next = @page + 1
-  	@page_back = @page > 1 ? @page - 1 : 1
   	
     @results = Search.search_for(@terms).uniq
     @count = @results.count
-
-    if !@browser.mobile?
-      @results = @results.paginate(page: @page, per_page: 9)
-    end
-  	@last_page = @results.length < 9
-    @first_page = @page < 2 
   end
 
   def index
   	@terms = params[:terms]
-    @og_title = "Search for \"#{@terms}\" - DraperTV"
-  	@page = params[:page] || 1
-    @page = @page.to_i
-  	@page_next = @page + 1
-  	@page_back = @page > 1 ? @page - 1 : 1
-  	
+    @og_title = "Search for \"#{@terms}\" - DraperTV"  	
     @results = Search.search_for(@terms).uniq
     @count = @results.count
-    @results = @results.paginate(page: @page, per_page: 9)
-	  @last_page = @results.length < 9
-    @first_page = @page < 2 
   	render 'create'
   end
 end
