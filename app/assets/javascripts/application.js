6015e0d6f2839101aa2e8688d01ec8730b847635
Andy Lee <alee@andy.local>
2015-08-13 16:04:18 -0700

    Remove unecessary js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,4 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require bootstrap-sprockets
-//= require react
-//= require react_ujs
-//= require components
 //= require_tree .
