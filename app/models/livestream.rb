0dbd20a770589c6432895859bf9f356647d3b220
Andy Lee <alee@andy.local>
2016-02-02 13:16:18 -0800

    fix date display

--- app/models/livestream.rb
+++ app/models/livestream.rb
@@ -39,13 +39,13 @@ class Livestream < ActiveRecord::Base
   def formatted_stream_date
   	pst_stream_date = stream_date - 8.hours
   	pst_current_time = Time.now.utc - 8.hours
-  	if stream_date - Time.now < 24.hours && pst_stream_date.day == pst_current_time.day
+  	if stream_date - Time.now < 24.hours && stream_date - Time.now > -24.hours && pst_stream_date.day == pst_current_time.day
       if stream_date < Time.now
         elapsed_time
       else
         pst_stream_date.strftime("Today at %l:%M %P PST")
       end
-  	elsif stream_date - Time.now < 48.hours && pst_stream_date.day == pst_current_time.day + 1
+  	elsif stream_date - Time.now < 48.hours &&  stream_date - Time.now > -24.hours && pst_stream_date.day == pst_current_time.day + 1
   		pst_stream_date.strftime("Tomorrow at %l:%M %P PST")
   	elsif stream_date - Time.now < 48.hours && stream_date > Time.now
       (stream_date - 8.hours).strftime("%B %-d, at %l:%M%P PST")
