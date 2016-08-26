5d5d36b9e1c2f98717650d5fc2491e0756ebf4c4
hzla00 <andylee.hzl@gmail.com>
2016-08-26 16:30:29 -0700

    fix video pages

--- app/uploaders/profilepic_uploader.rb
+++ app/uploaders/profilepic_uploader.rb
@@ -18,7 +18,9 @@ class ProfilepicUploader < CarrierWave::Uploader::Base
     "tony-photo.png"
   end
 
-  process :resize_to_fit => [340, 340]
+  version :square do
+    process :resize_to_fit => [340, 340]
+  end
 
 
 
